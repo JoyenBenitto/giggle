f5ca3dbdebb56d1a757099f56bba9b282b3a46dc
JoyenBenitto <pesug20ec028@pesu.pes.edu>
2024-06-01 19:19:47 +0530

    arg parse

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+    "flag"
+    "fmt"
+)
 
-func main(){
-   fmt.Println("Hello world")
+func main() {
+
+    config_yaml := flag.String("config_yaml", "./default_config.yaml", "Path to the config yaml")
+    build_dir := flag.String("build_dir", "./build", "Path to the build directory")
+
+    flag.Parse()
+
+    
+    fmt.Println("build_dir:", *build_dir)
+    fmt.Println("config:", *config_yaml)
 }
